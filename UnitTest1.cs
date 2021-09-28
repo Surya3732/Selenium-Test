6226d5c3b05a2bd8012d9a7624318688f4fa0fee
Surya3732 <91540117+Surya3732@users.noreply.github.com>
2021-09-28 17:47:47 +0530

    Test1

---
+++ UnitTest1.cs
@@ -0,0 +1,38 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace TestProject1
+{
+    public class Tests
+    {       
+        IWebDriver driver;
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void Test1()
+        {        
+            driver.Navigate().GoToUrl("https://www.youtube.com/");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);         
+            driver.FindElement(By.Name("search_query")).SendKeys("R15v2");
+            driver.FindElement(By.Id("search-icon-legacy")).Click();
+            Thread.Sleep(1000);
+            Screenshot srcFile = ((ITakesScreenshot)driver).GetScreenshot();
+            var screenShotPath = @"C:\\Users\\Surya\\source\\repos\\TestProject1\\TestProject1\\screenShot.PNG";
+            srcFile.SaveAsFile(screenShotPath);
+            Thread.Sleep(5000);
+        }
+        [TearDown]
+        public void TearDownMethod()
+        {
+            driver.Quit();
+        }
+    }
+}
